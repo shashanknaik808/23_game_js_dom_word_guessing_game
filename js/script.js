b2b501bde6b2bba4505fa79db0844380fb82b0de
Shashank Naik <shashanknaik808@gmail.com>
2023-10-13 16:37:27 +0530

    Getting the Random Word

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelector(".inputs"),
+    resetBtn = document.querySelector(".reset-btn"),
+    hintTag = document.querySelector(".hint span");
+
+
+function randomWord() {
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    hintTag.innerText = ranItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+randomWord();
+
+resetBtn.addEventListener("click", randomWord);
+document.addEventListener("keydown", () => typingInput.focus());
